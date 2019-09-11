303fcd30ba1604f357c936a56877d01362d668c2
Benjamin Perez <perez.benjamin@protonmail.com>
2019-09-11 04:41:31 +0200

    correcting little bug

--- GameEngine/main/myexe/include/ShaderSources.hpp
+++ GameEngine/main/myexe/include/ShaderSources.hpp
@@ -2,9 +2,9 @@ const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
 "void main()\n"
 "{\n"
-"   double x = 2.0*(aPos.x/800.0)-1;\n"
-"   double y = 2.0*(aPos.y/600.0)-1;\n"
-"   double z = aPos.z;\n"
+"   float x = 2.0*(aPos.x/800.0)-1;\n"
+"   float y = 2.0*(aPos.y/600.0)-1;\n"
+"   float z = aPos.z;\n"
 "   gl_Position = vec4(x, y, z, 1.0);\n"
 "}\0";
 
--- GameEngine/main/myexe/src/main.cpp
+++ GameEngine/main/myexe/src/main.cpp
@@ -204,7 +204,10 @@ std::tuple<unsigned int, unsigned int, unsigned int> createVAO(double x, double 
 
 	// set up vertex data (and buffer(s)) and configure vertex attributes
 	// ------------------------------------------------------------------
-	double vertices[] = { 
+	// The float generates a warning in the compilation because every thing 
+	// is in double until now. But for some reason, it doesn't work when we try
+	// to force opengl to use double
+	float vertices[] = { 
 	topX, topY, 0.0f, // top right
 	topX, bottomY, 0.0f,  // bottom right
 	bottomX, bottomY, 0.0f,  // bottom left
@@ -229,7 +232,7 @@ std::tuple<unsigned int, unsigned int, unsigned int> createVAO(double x, double 
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_DOUBLE, GL_FALSE, 3 * sizeof(double), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
