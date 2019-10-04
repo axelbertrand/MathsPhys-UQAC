bf35fbcb438c018d7f119cd907d16d44e41d06f4
Quentin Durand <quentin2.durand@wanadoo.fr>
2019-10-04 17:29:21 -0400

    Stiff spring addition, .vs/* added to .gitignore

--- GameEngine/main/physicslib/src/particleStiffSpringForceGenerator.cpp
+++ GameEngine/main/physicslib/src/particleStiffSpringForceGenerator.cpp
@@ -3,19 +3,17 @@
 
 namespace physicslib
 {
-	ParticleStiffSpringForceGenerator::ParticleStiffSpringForceGenerator(Vector3 anchorPosition, double elasticity, double damping) :
-
-	{
-
-	}
+	ParticleStiffSpringForceGenerator::ParticleStiffSpringForceGenerator
+	(Vector3 anchorPosition, double elasticity, double damping) :
+		m_anchorPosition(anchorPosition), m_elasticity(elasticity), m_damping(damping)
+	{}
 		
 	ParticleStiffSpringForceGenerator::~ParticleStiffSpringForceGenerator()
-	{
-
-	}
+	{}
 
 	ParticleStiffSpringForceGenerator::updateForce(Particle* particle, double duration)
 	{
-
+		Vector3 positionFromRest = particle->getPosition() - m_anchorPosition;
+		particle->addForce(- positionFromRest * m_elasticity - particle->getSpeed() * m_damping); // TODO à revoir
 	}
 }
