81af9d5aca0bc1eb40a26a4a6a15f058ebc88cb9
axelbertrand <axel.bertrand2@etu.univ-lyon1.fr>
2019-10-09 00:41:50 -0400

    fix ParticleRod

--- GameEngine/main/physicslib/src/collisions/particleRod.cpp
+++ GameEngine/main/physicslib/src/collisions/particleRod.cpp
@@ -27,6 +27,6 @@ namespace physicslib
 		}
 		Vector3 contactNormal = (m_particles[0]->getPosition() - m_particles[1]->getPosition()).getNormalizedVector() * direction;
 		double vs = contactNormal * (m_particles[0]->getSpeed() - m_particles[1]->getSpeed());
-		contactRegister.add(ParticleContact(m_particles[0], m_particles[1], 0., vs, 0., contactNormal));
+		contactRegister.add(ParticleContact(m_particles[0], m_particles[1], 1., vs, 0., contactNormal));
 	}
 }
