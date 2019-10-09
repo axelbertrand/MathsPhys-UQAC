190f86a3d458a01f9429c7738b21ab5308268a80
Benjamin Perez <perez.benjamin@protonmail.com>
2019-10-09 10:22:00 -0400

    water bug fix

--- GameEngine/main/physicslib/src/forceGenerator/particleBuoyancyForceGenerator.cpp
+++ GameEngine/main/physicslib/src/forceGenerator/particleBuoyancyForceGenerator.cpp
@@ -13,17 +13,16 @@ namespace physicslib
 	void ParticleBuoyancyForceGenerator::updateForce(std::shared_ptr<Particle> particle, const double duration) const
 	{
 		//the particle is aproximated by a cube of equal volume
-		double cubeEdge = pow(m_objectVolume, 1 / 3);
-		double submergedProportion = (particle->getPosition().getZ() - m_liquidHeight - cubeEdge/2.)/cubeEdge; // Doute, peut �tre l'oppos�
-		Vector3 zeroVector(0,1,0);
+		double cubeEdge = pow(m_objectVolume, 1.0 / 3.0);
+		double submergedProportion = -1 * (particle->getPosition().getY() - m_liquidHeight - cubeEdge / 2.) / cubeEdge;
+		Vector3 orientationVector(0, 1, 0);
 		if (submergedProportion <= 1 && submergedProportion >= 0)
 		{
-	
-			particle->addForce(zeroVector * submergedProportion * m_objectVolume * m_liquidDensity);
+			particle->addForce(orientationVector * submergedProportion * m_objectVolume * m_liquidDensity * duration);
 		}
 		else if (submergedProportion >= 1)
 		{
-			particle->addForce(zeroVector * m_objectVolume * m_liquidDensity);
+			particle->addForce(orientationVector * m_objectVolume * m_liquidDensity * duration);
 		}
 	}
 }
